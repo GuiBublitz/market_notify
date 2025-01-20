bb9329330b018b2b9fb4c273376528f954de596c
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2025-01-20 12:54:27 -0300

    add log of routines interval time

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const routines = require('./routines');
 const notify  = new Notify(whatsappClient);
 
 const intervalTime = process.env.NODE_ENV === 'production' ? 60000 * 3 : 10000;
+console.log('Routines Interval Time: ', intervalTime);
 
 setInterval(async () => {
     let whatsappState = await whatsappClient.getState();
